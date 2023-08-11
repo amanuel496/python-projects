a4b569586a357390c3c5f013024a2d21e2f1daf1
Amanuel Chukala <amanuelchukala@gmail.com>
2023-08-11 09:47:48 -0600

    Added a quiz-brain feature that it retrieves the next question, checks the answer and whether there are questions left or not

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,17 +1,23 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 def main():
     question_bank = []
-
     for item in question_data:
         text = item["text"]
         answer = item["answer"]
         question_and_answer = Question(text, answer)
         question_bank.append(question_and_answer)
 
-    print(question_bank[0].text, question_bank[1].answer)
+    quiz = QuizBrain(question_bank)
+
+    while quiz.still_has_question():
+        quiz.next_question()
+
+    print("You've completed the quiz")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
 
 
 main()
