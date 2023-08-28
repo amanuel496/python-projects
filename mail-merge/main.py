f0c7d156ebac331880cdd7918891246a15ae6bdf
Amanuel Chukala <amanuelchukala@gmail.com>
2023-08-27 21:11:15 -0600

    Replaced hard coded variable and constants

--- mail-merge/main.py
+++ mail-merge/main.py
@@ -1,19 +1,25 @@
+PLACEHOLDER = "[name]"
+INPUT_LETTER_LOCATION = "./Input/Letters/starting_letter.txt"
+INPUT_NAMES_LOCATION = "./Input/Names/invited_names.txt"
+OUTPUT_LETTER_BASE_LOCATION = "./Output/ReadyToSend"
+
+
 def main():
 
     # Read the generic letter and store it in a list
-    with open("./Input/Letters/starting_letter.txt") as file:
+    with open(INPUT_LETTER_LOCATION) as file:
         generic_letter = file.read()
 
     # Read the "invited names" and store them in another list
-    with open("./Input/Names/invited_names.txt") as file:
+    with open(INPUT_NAMES_LOCATION) as file:
         names_list = file.readlines()
 
     # Go through the names list and use each name to replace the name placeholder inside the generic letter
     actual_letter = []
     for i in range(len(names_list)):
         name = names_list[i].strip()
-        actual_letter.append(generic_letter.replace("[name]", name))
-        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as file:
+        actual_letter.append(generic_letter.replace(PLACEHOLDER, name))
+        with open(OUTPUT_LETTER_BASE_LOCATION + f"/letter_for_{name}.txt", "w") as file:
             file.write(actual_letter[i])
 
 
