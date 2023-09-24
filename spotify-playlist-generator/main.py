3fe01e95112741cb03c568297079a39f466fca8b
Amanuel Chukala <amanuelchukala@gmail.com>
2023-09-24 01:06:50 -0600

    Applied Beautiful-Soup web scrapper to scrap top 100 musics from billboard site

--- spotify-playlist-generator/main.py
+++ spotify-playlist-generator/main.py
@@ -1,5 +1,9 @@
+from music_data import get_data
+
+
 def main():
     user_request = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+    get_data(user_request)
 
 
 if __name__ == '__main__':
--- spotify-playlist-generator/music_data.py
+++ spotify-playlist-generator/music_data.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.billboard.com/charts/hot-100/2010-02-06/"
+base_url = "https://www.billboard.com/charts/hot-100/"
 
-response = requests.get(url=URL)
-soup = BeautifulSoup(response.text, "html.parser")
+def get_data(date):
+    response = requests.get(url=base_url + date)
+    soup = BeautifulSoup(response.text, "html.parser")
+    result = soup.select(selector="div ul li ul li h3")
 
+    music_titles = []
+    for title in result:
+        music_titles.append(title.string.strip())
 
-print(soup.select_one("#title-of-a-story").text)
-print(soup.select_one(".o-chart-results-list__item").text)
-# print(soup.find("li").text)
+    return music_titles
